f0e730f97537e6ca827299e6471ecdb0230b13b5
Toni Itkonen <toni.itkonen@icloud.com>
2021-12-23 09:16:49 +0200

    ShowingPhoto: - "Load Photo" button added - Loading photo from camera or photolibrary - Photo is loaded in "ExpenseDetailController"

--- My-Driving-Diary/Controllers/ExpenseDetailController.swift
+++ My-Driving-Diary/Controllers/ExpenseDetailController.swift
@@ -11,6 +11,9 @@ protocol ExpenseDelegate {
 
 class ExpenseDetailController: UIViewController {
     
+    // MARK: Photo Properties
+    var image: UIImage? /// Adding photo properties
+    ///
     // MARK: Properties
     let dateFormatter: DateFormatter = {
         let dateFormatter = DateFormatter()
@@ -83,11 +86,20 @@ class ExpenseDetailController: UIViewController {
     
     fileprivate var myImageView: UIImageView = {
         let photobox = UIImageView()
-        photobox.frame = CGRect(x: 20, y: 300, width: 300, height: 200)
+        photobox.frame = CGRect(x: 20, y: 360, width: 300, height: 200)
         photobox.backgroundColor = .lightGray
-        
         return photobox
     }()
+    
+    fileprivate var loadPhoto: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .orange
+        button.frame = CGRect(x: 20, y: 300, width: 100, height: 40)
+        button.setTitle("Load Photo", for: .normal)
+        button.addTarget(self, action: #selector(loadPhotoButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
     
     // MARK: Life Cycle
     override func viewDidLoad() {
@@ -144,13 +156,27 @@ class ExpenseDetailController: UIViewController {
         }
     }
     
+    // MARK: SHOW PHOTO
+    func show(image: UIImage) {
+        myImageView.image = image
+        myImageView.isHidden = false
+      //addPhotoLabel.text = ""
+        //imageHeight.constant = 260
+        //tableView.reloadData()
+    }
+    
     // MARK: Functions
+    @objc func loadPhotoButtonPressed() {
+        pickPhoto()
+    }
+    
     fileprivate func setupUI() {
         view.addSubview(dateLabel)
         view.addSubview(amount)
         view.addSubview(expenseType)
         view.addSubview(details)
         view.addSubview(myImageView)
+        view.addSubview(loadPhoto)
         
         dateLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 90).isActive = true
         dateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
@@ -172,5 +198,80 @@ class ExpenseDetailController: UIViewController {
         details.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -610).isActive = true
     }
 
+} // END
+
+// MARK: EXTENSION FOR ADDING PHOTOS
+extension ExpenseDetailController: UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    // MARK: - Image Helper Methods
+    func takePhotoWithCamera() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func choosePhotoFromLibrary() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func pickPhoto() {
+      if UIImagePickerController.isSourceTypeAvailable(.camera) {
+        showPhotoMenu()
+      } else {
+        choosePhotoFromLibrary()
+      }
+    }
+
+    func showPhotoMenu() {
+      let alert = UIAlertController(
+        title: nil,
+        message: nil,
+        preferredStyle: .actionSheet)
+
+      let actCancel = UIAlertAction(
+        title: "Cancel",
+        style: .cancel,
+        handler: nil)
+      alert.addAction(actCancel)
+
+      let actPhoto = UIAlertAction(
+        title: "Take Photo",
+        style: .default) { _ in
+        self.takePhotoWithCamera()
+      }
+        
+      alert.addAction(actPhoto)
+
+      let actLibrary = UIAlertAction(
+        title: "Choose From Library",
+        style: .default) { _ in
+          self.choosePhotoFromLibrary()
+        }
+      alert.addAction(actLibrary)
+
+      present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - Image Picker Delegates
+    ///This is for showing photo in view
+    func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+          if let theImage = image {
+            show(image: theImage) /// Calls show photo function
+          }
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
+
 
